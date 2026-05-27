ec726493e337c9241c0e13a1f60a4d8fe8f70a90
justinocarloschemane <justinocarloschemane@gmail.com>
2026-05-27 09:14:24 +0200

    config: use environment variables for deployment

--- src/main/java/mz/payments/payment_api/PaymentApiApplication.java
+++ src/main/java/mz/payments/payment_api/PaymentApiApplication.java
@@ -3,6 +3,8 @@ package mz.payments.payment_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class PaymentApiApplication {
 
